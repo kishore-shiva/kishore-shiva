16926c256e9d78e2d6f191765a7f55541adb5ade
kishore-shiva <62393363+kishore-shiva@users.noreply.github.com>
2020-03-19 21:29:50 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,39 +1,82 @@
-import cv2
+#importing modules
+
+import cv2   
 import numpy as np
 
-cap = cv2.VideoCapture(0)
+#capturing video through webcam
+cap=cv2.VideoCapture(0)
 
-while True:
-    _, frame = cap.read()
-    hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    # Red color
-    low_red = np.array([161, 155, 84])
-    high_red = np.array([179, 255, 255])
-    red_mask = cv2.inRange(hsv_frame, low_red, high_red)
-    red = cv2.bitwise_and(frame, frame, mask=red_mask)
-    # Blue color
-    low_blue = np.array([94, 80, 2])
-    high_blue = np.array([126, 255, 255])
-    blue_mask = cv2.inRange(hsv_frame, low_blue, high_blue)
-    blue = cv2.bitwise_and(frame, frame, mask=blue_mask)
+while(1):
+	_, img = cap.read()
+	    
+	#converting frame(img i.e BGR) to HSV (hue-saturation-value)
 
-    # Green color
-    low_green = np.array([25, 52, 72])
-    high_green = np.array([102, 255, 255])
-    green_mask = cv2.inRange(hsv_frame, low_green, high_green)
-    green = cv2.bitwise_and(frame, frame, mask=green_mask)
+	hsv=cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
 
-    # Every color except white
-    low = np.array([0, 42, 0])
-    high = np.array([179, 255, 255])
-    mask = cv2.inRange(hsv_frame, low, high)
-    result = cv2.bitwise_and(frame, frame, mask=mask)
-    cv2.imshow("Frame", frame)
-    cv2.imshow("Red", red)
-    cv2.imshow("Blue", blue)
-    cv2.imshow("Green", green)
-    cv2.imshow("Result", result)
+	#definig the range of red color
+	red_lower=np.array([136,87,111],np.uint8)
+	red_upper=np.array([180,255,255],np.uint8)
 
-    key = cv2.waitKey(1)
-    if key == 27:
+	#defining the Range of Blue color
+	blue_lower=np.array([99,115,150],np.uint8)
+	blue_upper=np.array([110,255,255],np.uint8)
+	
+	#defining the Range of yellow color
+	yellow_lower=np.array([22,60,200],np.uint8)
+	yellow_upper=np.array([60,255,255],np.uint8)
+
+	#finding the range of red,blue and yellow color in the image
+	red=cv2.inRange(hsv, red_lower, red_upper)
+	blue=cv2.inRange(hsv,blue_lower,blue_upper)
+	yellow=cv2.inRange(hsv,yellow_lower,yellow_upper)
+	
+	#Morphological transformation, Dilation  	
+	kernal = np.ones((5 ,5), "uint8")
+
+red=cv2.dilate(red, kernal)
+res=cv2.bitwise_and(img, img, mask = red)
+
+blue=cv2.dilate(blue,kernal)
+res1=cv2.bitwise_and(img, img, mask = blue)
+
+yellow=cv2.dilate(yellow,kernal)
+res2=cv2.bitwise_and(img, img, mask = yellow)    
+
+
+#Tracking the Red Color
+(_,contours,hierarchy)=cv2.findContours(red,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+
+for pic, contour in enumerate(contours):
+    area = cv2.contourArea(contour)
+    if(area>300):
+
+        x,y,w,h = cv2.boundingRect(contour)	
+        img = cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
+        cv2.putText(img,"RED color",(x,y),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,255))
+
+#Tracking the Blue Color
+(_,contours,hierarchy)=cv2.findContours(blue,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+for pic, contour in enumerate(contours):
+    area = cv2.contourArea(contour)
+    if(area>300):
+        x,y,w,h = cv2.boundingRect(contour)	
+        img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        cv2.putText(img,"Blue color",(x,y),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255,0,0))
+
+#Tracking the yellow Color
+(_,contours,hierarchy)=cv2.findContours(yellow,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+for pic, contour in enumerate(contours):
+    area = cv2.contourArea(contour)
+    if(area>300):
+        x,y,w,h = cv2.boundingRect(contour)	
+        img = cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+        cv2.putText(img,"yellow  color",(x,y),cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0,255,0))  
+
+
+    #cv2.imshow("Redcolour",red)
+    cv2.imshow("Color Tracking",img)
+    #cv2.imshow("red",res) 	
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        cap.release()
+        cv2.destroyAllWindows()
         break
